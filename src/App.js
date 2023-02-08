9ae6af1f3fa9e83406cc3ee8a60fd6ad003a1b80
adityabhoir777 <adityabhoir@mailfence.com>
2023-02-08 10:56:29 +0530

    finished working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import Loader from 'react-loader';
+import { useEffect, useState } from 'react'
+import './App.css'
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+  const [card, setCard] = useState({});
+  const [loaded, setLoaded] = useState(false);
+
+  async function fetchUsers() {
+    let { data } = await axios.get('https://reqres.in/api/users');
+    if (data) {
+      setLoaded(true);
+      let _users = data.data;
+      setUsers(_users);
+    }
+  }
+
+  async function fetchUser(id) {
+    setLoaded(false);
+    let response = await axios.get(`https://reqres.in/api/users/${id}`);
+    if (response) {
+      setLoaded(true);
+      setCard(response.data.data);
+    }
+  }
+
+  useEffect(() => {
+    fetchUsers();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Loader loaded={loaded}>
+      <div className="App">
+        <div className='card'>
+          {
+            card.id
+              ?
+              <>
+                <div>
+                  <img src={card.avatar} alt={card.first_name} />
+                </div>
+                <div>
+                  {card.first_name} {card.last_name}
+                </div>
+                <div>
+                  {card.email}
+                </div>
+              </>
+              : <div> Please click on any Button </div>
+
+          }
+        </div>
+        <div className='buttons'>
+          {
+            users.map(user => {
+              return (
+                <button key={user.email} onClick={() => fetchUser(user.id)}>
+                  {user.id}
+                </button>
+              )
+            })
+          }
+        </div>
+      </div>
+    </Loader>
+  )
 }
 
-export default App;
+export default App
